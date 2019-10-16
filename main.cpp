be96cc722b1308a49533af5f0197580bf34c22c1
LadanyiPatrisia2003 <47587954+LadanyiPatrisia2003@users.noreply.github.com>
2019-10-16 09:23:59 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a,oszto;
+    bool prim= true;
+    cout << "a=";
+    cin >> a;
+    for (oszto=2; oszto<a/2+1; oszto++){
+     if (a%oszto==0) {
+        prim=false;
+     }
+    }
+    if (prim) {
+        cout << " a szam prim" ;
+    } else {
+        cout << "a szam nem prim";
+    }
+
     return 0;
 }
